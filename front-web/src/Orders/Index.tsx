b893ffe6f280827a306b889e8089092f25179215
Heliks <heliksmm@hotmail.com>
2021-01-09 15:33:10 -0300

    Add product tables in local host

--- front-web/src/Orders/Index.tsx
+++ front-web/src/Orders/Index.tsx
@@ -1,12 +1,26 @@
 import './Styles.css';
 import StepsHeader from './StepsHeader';
+import ProductsList from './ProductsList';
+import { useEffect, useState } from 'react';
+import { Product } from './Types';
+import { fetchProducts } from '../api';
 //import {ReactComponent as Logo} from './Logo.svg';
 
 function Orders(){
+    const [products,setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(() => {
+        fetchProducts()
+        .then(response => setProducts(response.data))
+        .catch(error => console.log(error))
+    },[])
+
     return (
         <div className="orders-container">
             <StepsHeader />
-        </div>
+            <ProductsList products={products} />
+        </div> 
     )
 }
 
