e33e626c462d043f3e47b76927f71d3651d110fe
Heliks <heliksmm@hotmail.com>
2021-01-09 20:39:03 -0300

    Search order location

--- front-web/src/Orders/Index.tsx
+++ front-web/src/Orders/Index.tsx
@@ -1,13 +1,16 @@
 import './Styles.css';
 import StepsHeader from './StepsHeader';
 import ProductsList from './ProductsList';
+import OrderLocation from './OrderLocation';
 import { useEffect, useState } from 'react';
-import { Product } from './Types';
+import { OrderLocationData, Product } from './Types';
 import { fetchProducts } from '../api';
 //import {ReactComponent as Logo} from './Logo.svg';
 
 function Orders(){
     const [products,setProducts] = useState<Product[]>([]);
+    const [orderLocation,setOrderLocation] = useState<OrderLocationData>();
+
     console.log(products);
 
     useEffect(() => {
@@ -20,6 +23,7 @@ function Orders(){
         <div className="orders-container">
             <StepsHeader />
             <ProductsList products={products} />
+            <OrderLocation onChangeLocation={location=> setOrderLocation(location)} />
         </div> 
     )
 }
