e84b16f56c1a4b6ce3a162310a7f65e2e1d516b5
Heliks <heliksmm@hotmail.com>
2021-01-10 13:49:01 -0300

    Add env vars

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { OrderPayLoad } from './Orders/Types';
 
-const API_URL = 'http://localhost:8080';
+const API_URL = process.env.REACT_APP_API_URL;
 
 const mapboxToken =process.env.REACT_APP_ACCESS_TOKEN_MAPBOX;
 
